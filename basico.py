5ef0c4d42425a0a97f9d39be14a8b1d4a49ec244
Isabelle Tschoeke Volaco <111777240+IsabelleVolaco@users.noreply.github.com>
2024-04-24 19:47:38 -0300

    lógica py

---
+++ basico.py
@@ -0,0 +1,29 @@
+country = "Brasil"
+state = "Santa Catarina"
+city = "Blumenau"
+
+numero = 500
+exemplo_do_numero = numero * 2
+
+numero = 200
+numero_dois = 300
+exemplo_do_numero_dois = numero + numero_dois
+print(exemplo_do_numero_dois)
+
+x = input()
+print(x + "o user adiciona o valor")
+
+is_day = False
+lights_on = not is_day
+print("Daytime?" + str(is_day))
+print("Lights on?" + str(lights_on))
+
+estoque = 100
+camisas_vendidas = 50
+quantidade_alvo = 50
+
+meta_atingida_alvo = camisas_vendidas == quantidade_alvo
+print("A meta de vendas foi atingida?" + str(meta_atingida_alvo))
+
+estoque_atual = estoque - camisas_vendidas
+print (estoque_atual)
