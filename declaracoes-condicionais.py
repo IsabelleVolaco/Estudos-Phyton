a962f8a3f3b7920b3b9e55a2f21989c134096ac0
Isabelle Tschoeke Volaco <111777240+IsabelleVolaco@users.noreply.github.com>
2024-04-26 15:00:06 -0300

    Update exercício de pontos

--- declaracoes-condicionais.py
+++ declaracoes-condicionais.py
@@ -24,3 +24,34 @@ print(f"Ride price is: {ride_price}. Select credits option.")
 if credits > 0:
     final_price = ride_price - credits
 print(f"Credit applied! Final price: {final_price}")
+
+print("Exercício de pontos")
+
+minimum_points = 150
+data_points = 100
+if data_points >= minimum_points:
+    print("Você pode comprar o item!")
+if data_points < minimum_points:
+    print(f"Você não tem pontos o suficiente! Faltam {minimum_points - data_points} pontos.")
+
+print("Exercícios de permissão")
+
+age = 17
+has_permit = True
+
+if age > 16 and has_permit:
+    print("Can drive in US")
+
+age = 17
+has_permit = True
+
+if age > 18 and has_permit == True:
+    print("Can drive in Brazil")
+
+age = 17
+has_permit = True
+is_insured = True
+
+if age > 16 and has_permit and is_insured:
+    print("Can drive. Os blocos de código serão pulados caso todos os requisitos não sejam cumpridos.")
+
