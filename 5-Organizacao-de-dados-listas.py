c3de193ab7f49828406e1242f26d4f7b8d5d1aab
Isabelle Tschoeke Volaco <111777240+IsabelleVolaco@users.noreply.github.com>
2024-04-30 12:24:12 -0300

    listas

--- 5-Organizacao-de-dados-listas.py
+++ 5-Organizacao-de-dados-listas.py
@@ -1,1 +1,16 @@
+"""Exemplo de listas"""
 
+pizza = ["pepperoni","siciliana","milho com bacon"]
+print(pizza)
+
+pizza = ["pepperoni","siciliana","milho com bacon"]
+print(pizza[0])
+
+pizza = ["pepperoni","siciliana","milho com bacon"]
+print(pizza[2])
+
+pizza = ["pepperoni","siciliana","milho com bacon"]
+pizza[2] = "4 queijos"
+print(pizza)
+
+#Os valores de uma lista são chamados de elementos. A posição dos elementos na lista é o índice (0,1,2 = 3).
