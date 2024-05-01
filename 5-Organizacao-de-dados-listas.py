a4ba9048ed742ea52a35108fab66c45614d16379
Isabelle Tschoeke Volaco <111777240+IsabelleVolaco@users.noreply.github.com>
2024-05-01 11:01:08 -0300

    instrucoes

--- 5-Organizacao-de-dados-listas.py
+++ 5-Organizacao-de-dados-listas.py
@@ -14,3 +14,15 @@ pizza[2] = "4 queijos"
 print(pizza)
 
 #Os valores de uma lista são chamados de elementos. A posição dos elementos na lista é o índice (0,1,2 = 3).
+
+print("exercícios de instruções")
+
+mercado = []
+mercado.append("tomate")
+print(mercado)
+
+mercado.insert(0, "leite")
+print(mercado)
+
+mercado.pop(1)
+print(mercado)
