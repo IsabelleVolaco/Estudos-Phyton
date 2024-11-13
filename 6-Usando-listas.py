6ad89767ed201c855d31c83e1783319e7a9ce579
Isabelle Tschoeke Volaco <111777240+IsabelleVolaco@users.noreply.github.com>
2024-11-13 15:28:13 -0300

    ex call center

--- 6-Usando-listas.py
+++ 6-Usando-listas.py
@@ -442,3 +442,35 @@ print("During final minutes: " + str(final_mins))
 max_index = bpm.index(max(bpm))
 print("Recovery from maximum: " + str(bpm[max_index:]))
 
+
+
+#------------------------------------------------------------
+print("Exercício alocador de call center")
+
+calls = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
+staff = ["Anna", "Ben", "Cherry"]
+staff_count = len(staff)
+early_staff = 2
+early_calls = calls[:5]
+
+early_allocation = {}
+for employee in staff[:early_staff]:
+    position = staff.index(employee)
+    early_allocation[employee] = early_calls[position::early_staff]
+    
+print(early_allocation)
+
+late_calls = calls[5:]
+late_staff = staff[::-1]
+late_allocation = {}
+
+for employee in late_staff:
+    position = late_staff.index(employee)
+    late_allocation[employee] = late_calls[position::staff_count]
+    
+print(late_allocation)
+
+      
+      
+    
